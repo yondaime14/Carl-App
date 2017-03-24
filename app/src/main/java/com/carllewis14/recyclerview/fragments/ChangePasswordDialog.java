4c7d0eac88357f3ef52edcccadecd72f8144bc64
carl Lewis <carl.lewis@incisivemedia.com>
2017-03-24 16:35:29 +0000

    added changed dialog fragment for signup

--- app/src/main/java/com/carllewis14/recyclerview/fragments/ChangePasswordDialog.java
+++ app/src/main/java/com/carllewis14/recyclerview/fragments/ChangePasswordDialog.java
@@ -1,111 +1,109 @@
 package com.carllewis14.recyclerview.fragments;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.carllewis14.recyclerview.Activities.ProfileActivity;
 import com.carllewis14.recyclerview.R;
+import com.carllewis14.recyclerview.utils.Constants;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ChangePasswordDialog.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ChangePasswordDialog#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ChangePasswordDialog extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import rx.subscriptions.CompositeSubscription;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
+public class ChangePasswordDialog extends DialogFragment {
 
-    public ChangePasswordDialog() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChangePasswordDialog.
+    /*
+    Callback interface
+     will notify parent activity when process is complete
      */
-    // TODO: Rename and change types and number of parameters
-    public static ChangePasswordDialog newInstance(String param1, String param2) {
-        ChangePasswordDialog fragment = new ChangePasswordDialog();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+
+    public interface Listener {
+        void onPasswordChanged();
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public static final String TAG = ChangePasswordDialog.class.getSimpleName();
+
+    private EditText mEtOldPassword;
+    private EditText mEtNewPassword;
+    private Button mBtChangedPassword;
+    private Button mBtcancel;
+    private TextView mTvMessage;
+    private TextInputLayout mTiOldPassword;
+    private TextInputLayout mTiNewPassword;
+    private ProgressBar mProgressBar;
+
+    private CompositeSubscription compositeSubscription;
+    private String mToken;
+    private String mEmail;
+    private Listener mListener;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+
+        View view = inflater.inflate(R.layout.dialog_change_password, container, false);
+        compositeSubscription = new CompositeSubscription();
+        getData();
+        initViews(view);
+
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+
+    private void getData() {
+
+        Bundle bundle = getArguments();
+
+        mToken = bundle.getString(Constants.TOKEN);
+        mEmail = bundle.getString(Constants.EMAIL);
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+        mListener = (ProfileActivity)context;
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    private void initViews(View view) {
+
+        mEtOldPassword = (EditText) view.findViewById(R.id.et_old_password);
+        mEtNewPassword = (EditText) view.findViewById(R.id.et_new_password);
+        mTiOldPassword = (TextInputLayout) view.findViewById(R.id.ti_old_password);
+        mTiNewPassword = (TextInputLayout) view.findViewById(R.id.ti_new_password);
+
+        mTvMessage = (TextView) view.findViewById(R.id.tv_message);
+        mBtChangedPassword = (Button) view.findViewById(R.id.btn_change_password);
+        mBtcancel = (Button) view.findViewById(R.id.btn_cancel);
+        mProgressBar = (ProgressBar) view.findViewById(R.id.progress);
+
+        mBtChangedPassword.setOnClickListener(view -> changePassword());
+        mBtcancel.setOnClickListener(view -> dismiss());
+
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    private void changePassword() {
+
+        setError();
+
+        String oldPassword = mEtOldPassword.getText().toString();
+        String newPassword = mEtNewPassword.getText().toString();
+
+        int invalid = 0;
+
+        
     }
+
+    private void setError() {
+    }
+
+
 }
